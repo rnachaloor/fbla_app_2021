54fe0017b00c5efe350b4a5ee7f2683a355b74d9
rnachaloor <rnachaloor@gmail.com>
2020-02-08 17:35:12 -0500

    added menu to officer page

--- app/src/main/java/com/example/fblaapp/Officers.java
+++ app/src/main/java/com/example/fblaapp/Officers.java
@@ -1,10 +1,30 @@
 package com.example.fblaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Officers extends AppCompatActivity {
+
+    public boolean socialMediaLinks(MenuItem item) {
+        Intent intent = new Intent(this, SocialMediaLinks.class);
+        startActivity(intent);
+        return true;
+    }
+
+    public boolean officerPage(MenuItem item) {
+        Intent intent = new Intent(this, Officers.class);
+        startActivity(intent);
+        return true;
+    }
+
+    public boolean homePage(MenuItem item) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
