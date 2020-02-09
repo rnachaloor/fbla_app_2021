c8a6854157254701264bcefe24725a3c27b4ec3d
rnachaloor <rnachaloor@gmail.com>
2020-02-09 00:18:38 -0500

    finished join form

---
+++ app/src/main/java/com/example/fblaapp/CurrentEventForm.java
@@ -0,0 +1,14 @@
+package com.example.fblaapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CurrentEventForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_event_form);
+    }
+}
