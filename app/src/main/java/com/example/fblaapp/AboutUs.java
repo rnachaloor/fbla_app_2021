8478682f5fccbd6f66af069c42e86187cf8f933d
rnachaloor <rnachaloor@gmail.com>
2020-02-08 19:01:09 -0500

    finished AboutUs Activity

---
+++ app/src/main/java/com/example/fblaapp/AboutUs.java
@@ -0,0 +1,49 @@
+package com.example.fblaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutUs extends AppCompatActivity {
+
+    public boolean socialMediaLinks(MenuItem item) {
+        Intent intent = new Intent(this, SocialMediaLinks.class);
+        startActivity(intent);
+        return true;
+    }
+
+    public boolean homePage(MenuItem item) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
+    public boolean officerPage(MenuItem item) {
+        Intent intent = new Intent(this, Officers.class);
+        startActivity(intent);
+        return true;
+    }
+
+    public boolean aboutUs(MenuItem item) {
+        Intent intent = new Intent(this, AboutUs.class);
+        startActivity(intent);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
